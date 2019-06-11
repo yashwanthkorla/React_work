bcdba8daaafc2156f67dd736b95cb4399a326d0c
yashwanthkorla <yashwanth.korla@accenture.com>
2019-06-11 11:56:48 +0530

    Modified the filter string in checkAndDispatch

--- Main/helperFunctions/forTodoProject/checkAndDispatch.js
+++ Main/helperFunctions/forTodoProject/checkAndDispatch.js
@@ -1,6 +1,6 @@
 import {add_goal,add_todo} from './constants.js'
 // use this helper when u want to filter some task before updating the store.
-const filter_value = "<Your filter value like stupid in the value shouldnt dispatch>"
+const filter_value = "<Your filter value like a task or goal with stupid in the action value shouldnt dispatch>"
 export default function checkAndDispatch(store,action){
     if(action.type === add_goal && action.goal.goalValue.toLowerCase().includes(filter_value) ){
         return alert(`You cant add this goal with '${filter_value}' in it`)
