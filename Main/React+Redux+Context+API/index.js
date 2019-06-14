4eafe0fb7b0edba7f997f4de803cbe64d334827c
yashwanthkorla <yashwanth.korla@accenture.com>
2019-06-14 11:26:48 +0530

    Added some description and create react+redux+context+api folder

---
+++ Main/React+Redux+Context+API/index.js
@@ -0,0 +1,9 @@
+class App extends React.Component{
+    render(){
+        return (
+            <p>asd</p>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("contextapiexample"))
