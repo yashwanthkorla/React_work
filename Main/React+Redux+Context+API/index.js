7509a46d7a4ed60d7d6eeedbc19a594d0d535b91
yashwanthkorla <yashwanth.korla@accenture.com>
2019-06-18 13:00:15 +0530

    Creating Tweet App - udacity nano degree project

--- Main/React+Redux+Context+API/index.js
+++ Main/React+Redux+Context+API/index.js
@@ -1,9 +1,30 @@
-class App extends React.Component{
-    render(){
-        return (
-            <p>asd</p>
-        )
-    }
+const contextAPI = React.createContext();
+function Description() {
+  return (
+    <contextAPI.Consumer>
+      {(name) =>
+      (
+        <div>
+          <p>{name}</p>
+        </div>
+      )
+      }
+    </contextAPI.Consumer>
+  );
+}
+class App extends React.Component {
+  render() {
+    const name = "yashwanth";
+    const role = "developer";
+    return (
+      <contextAPI.Provider value={name}>
+        <div>
+          <p>Hey there</p>
+          <Description />
+        </div>
+      </contextAPI.Provider>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById("contextapiexample"))
+ReactDOM.render(<App />, document.getElementById("contextapiexample"));
---
+++ Main/tweet-app/README.md
@@ -0,0 +1,1 @@
+This project is from react nanodegree by udacity.
